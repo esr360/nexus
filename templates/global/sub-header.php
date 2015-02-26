32b0a03ff9b4ee1b1782e751044bdcfb01842da2
Ed Reed <esr360@live.com>
2015-02-26 21:05:18 +0000

    remove icons from buttons

--- templates/global/sub-header.php
+++ templates/global/sub-header.php
@@ -3,7 +3,7 @@
         <div class="sh-title-wrapper">
             <h1><?php echo $title; ?></h1>
             <p>Aenean lobortis ante nunc. Integer ac justo ex. Curabitur at enim ac nisi lacinia.</p>
-            <a class="button border round cta" href="#"><i class="fa fa-shopping-cart"></i> Purchase Theme</a>
+            <a class="button border round cta" href="#">Purchase Theme</a>
         </div>
     </div>
 </section>
