623e68283ad7e3fa2e3a5575fb85f5e0c6234074
Ed Reed <esr360@live.com>
2015-03-04 20:44:55 +0000

    add active animations to carousel nav icons

--- templates/sections/services.php
+++ templates/sections/services.php
@@ -1,4 +1,4 @@
-<section class="section services primary section-map" id="s-services">
+<section class="section services primary section-map inactive" id="s-services">
     <div class="container">
         <header class="sep active">
             <div class="section-title">
@@ -6,7 +6,7 @@
             </div>
             <p class="sub-text">Suspendisse tempus sodales neque, eget eleifend <a href="#">turpis tristique</a> eu. Nullam a nisl maximus, ultrices est ut blandit nislr, elit in lobortis mattis.</p>
         </header>
-        <div class="owl-carousel services-slider inactive">
+        <div class="owl-carousel services-slider">
             <div class="service">
                 <i class="livicon" data-n="desktop" data-op="1" data-c="#C1C1C1" data-s="55" data-hc="false"></i>
                 <div class="title">
