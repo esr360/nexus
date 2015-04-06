3e13029d853f20f7ce8594118d9cc6d0bbc80745
Edmund <esr360@live.com>
2015-04-06 03:12:18 +0100

    creating master.php file

---
+++ templates/pages/master.php
@@ -0,0 +1,16 @@
+<?php require("about.php"); ?>
+<?php require("blog.php"); ?>
+<?php require("contact.php"); ?>
+<?php require("index.php"); ?>
+<?php require("index-2.php"); ?>
+<?php require("index-3.php"); ?>
+<?php require("index-4.php"); ?>
+<?php require("index-single.php"); ?>
+<?php require("index-single-2.php"); ?>
+<?php require("index-single-3.php"); ?>
+<?php require("index-single-4.php"); ?>
+<?php require("landing.php"); ?>
+<?php require("portfolio.php"); ?>
+<?php require("services.php"); ?>
+<?php require("single-post.php"); ?>
+<?php require("single-project.php"); ?>
